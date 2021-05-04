52d98254f6584b6d59e98b74fd0e0124f49284d2
devkaan <abdikaangrbz@gmail.com>
2021-05-04 11:47:13 +0300

    first commit

---
+++ webcam.py
@@ -0,0 +1,39 @@
+import cv2
+import sys
+
+cascPath = "haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascPath)
+
+video_capture = cv2.VideoCapture(0)
+
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=10,
+        minSize=(30, 30)
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 200, 0), 2)
+
+    # Display the resulting frame
+    flippedFrame = cv2.flip(frame,1)
+    cv2.imshow('Video', flippedFrame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        
+
+
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
